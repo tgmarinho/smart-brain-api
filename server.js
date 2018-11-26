b5a7f072d3c203bbf98d775b308020ea52aadd63
Thiago Marinho <tgmarinho@gmail.com>
2018-11-26 19:31:31 -0300

    implemented api of login

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -11,7 +12,6 @@ const database = {
       id: '123',
       name: 'Jhon',
       email: 'jhon@gmail.com',
-      password: 'cookies',
       entries: 0,
       joined: new Date()
     },
@@ -19,9 +19,15 @@ const database = {
       id: '124',
       name: 'sally',
       email: 'sally@gmail.com',
-      password: 'bananas',
       entries: 0,
       joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'jhon@gmail.com'
     }
   ]
 }
@@ -31,11 +37,10 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
- console.log(req)
-  console.log(req.body.email);
- console.log(req.body.password);
 
-
+  bcrypt.compare("apples", null, null, function(err, hash) {
+    console.log(hash)
+   });
   if (req.body.email === database.users[0].email
     && req.body.password === database.users[0].password) {
     res.json('sucess')
@@ -48,23 +53,63 @@ app.post('/signin', (req, res) => {
 
 
 app.post('/register', (req, res) => {
-  console.log(req.body.email);
- console.log(req.body.password);
+
   const { email, name, password } = req.body;
-  database.users.push({
-  id: '125',
-  name,
-  email,
-  password,
-  entries: 0,
-  joined: new Date()
-})
+
+      database.users.push({
+      id: '125',
+      name,
+      email,
+      password,
+      entries: 0,
+      joined: new Date()
+    })
 res.json(database.users[database.users.length - 1])
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true;
+      return res.json(user)
+    }
+  })
+  if(!found) {
+      res.status(404).json('no such user');
+  }
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries)
+    }
+  })
+  if(!found) {
+      res.status(404).json('no such user');
+  }
 })
 
 app.listen(3000, () => {
   console.log('App is runing on port 3000!!!')
 });
+
+
+
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
 
 
 /*
