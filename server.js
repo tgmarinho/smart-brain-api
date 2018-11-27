5cbcce1828e317d4542c79d61a08c9c5065461a1
Thiago Marinho <tgmarinho@gmail.com>
2018-11-27 17:16:18 -0300

    registering a new user and adding to database and returning him for our frontend

--- server.js
+++ server.js
@@ -23,46 +23,17 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors())
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'Jhon',
-      email: 'jhon@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'sally',
-      password: 'bananas',
-      email: 'sally@gmail.com',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'jhon@gmail.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
   res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-console.log(req.body)
   // bcrypt.compare("apples", null, null, function(err, hash) {
   //   console.log(hash)
   //  });
   if (req.body.email === database.users[0].email
     && req.body.password === database.users[0].password) {
-      res.json(database.users[0]);
+    res.json(database.users[0]);
 
   } else {
     res.status(400).json('error loggin in')
@@ -72,30 +43,29 @@ console.log(req.body)
 
 
 app.post('/register', (req, res) => {
-
   const { email, name, password } = req.body;
+  db('users')
+  .returning('*')
+  .insert({
+    email, name, joined: new Date()
+  }).then(user => {
+    res.json(user[0])
+  }).catch(err => res.status(400).json('unable to register'))
 
-      database.users.push({
-      id: '125',
-      name,
-      email,
-      entries: 0,
-      joined: new Date()
-    })
-res.json(database.users[database.users.length - 1])
+
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
   let found = false;
   database.users.forEach(user => {
-    if(user.id === id) {
+    if (user.id === id) {
       found = true;
       return res.json(user)
     }
   })
-  if(!found) {
-      res.status(404).json('no such user');
+  if (!found) {
+    res.status(404).json('no such user');
   }
 });
 
@@ -103,14 +73,14 @@ app.put('/image', (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach(user => {
-    if(user.id === id) {
+    if (user.id === id) {
       found = true;
       user.entries++;
       return res.json(user.entries)
     }
   })
-  if(!found) {
-      res.status(404).json('no such user');
+  if (!found) {
+    res.status(404).json('no such user');
   }
 })
 
